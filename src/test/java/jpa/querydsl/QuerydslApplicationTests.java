3217d36423ac98f8887b65379ca4cc90a2b97abe
ggssww99 <ggssww99@gmail.com>
2023-05-06 15:24:08 +0900

    querydsl 설정 추가

--- src/test/java/jpa/querydsl/QuerydslApplicationTests.java
+++ src/test/java/jpa/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package jpa.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jpa.querydsl.entity.Hello;
+import jpa.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        //QHello qHello = new QHello("h");
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
