f057f430beed8928c54a9d3dd1ce8d25f01e1b59
ggssww99 <ggssww99@gmail.com>
2023-05-06 16:44:09 +0900

    querydsl 설정 추가 p6spy 설정 추가

--- src/test/java/jpa/querydsl/QuerydslApplicationTests.java
+++ src/test/java/jpa/querydsl/QuerydslApplicationTests.java
@@ -8,11 +8,13 @@ import jpa.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
     @Autowired
